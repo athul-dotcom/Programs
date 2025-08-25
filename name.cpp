87dcf18612fad1d401ba39278eddad94daa45dcd
Athul K R <athulkr34@gmail.com>
2025-08-25 23:30:07 +0530

    Add files via upload

---
+++ name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char name[50];
+    cout<<"Enter name: ";
+    cin>>name;
+
+    cout<<"Namah Shivaya, "<<name<<"!" <<endl;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter 1st number:";
+    cin>>a;
+
+    cout<<"Enter 2nd number:";
+    cin>>b;
+
+    sum=a+b;
+    cout<<"The sum of " <<a<< " and " <<b<< " is:" <<sum<<endl;
+    return 0;
+}
