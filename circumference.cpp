4b829983bf529d374251e3f123ee5e65f747cded
Athul K R <athulkr34@gmail.com>
2025-08-26 21:41:33 +0530

    Add files via upload

---
+++ circumference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float c,r;
+
+    cout<<"Enter radius value:";
+    cin>>r;
+
+    cout<<"Taking PI value as 22/7"<<endl;
+
+    c=(2*22/7*r);
+    cout<<"Circumference of circle is:"<<c<<endl;
+    return 0;
+}
