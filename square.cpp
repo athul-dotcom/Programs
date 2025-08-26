47b1dc6f029bab2f20925db54e5f71ace55eb708
Athul K R <athulkr34@gmail.com>
2025-08-26 21:33:12 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,sq;
+    cout<<"Enter a number:";
+    cin>>a;
+
+    sq=a*a;
+
+    cout<<"Square of "<<a<<" number is:"<<sq<<endl;
+    return 0;
+}
