92c1c48cf5c077f5b87de30014262c3f75bbbc09
Athul K R <athulkr34@gmail.com>
2025-08-26 21:34:36 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,cube;
+    cout<<"Enter a number:";
+    cin>>a;
+
+    cube=a*a*a;
+
+    cout<<"Square of "<<a<<" number is:"<<cube<<endl;
+    return 0;
+}
