60f31387b7e6052b5820e5f464a58ea5f49c1633
Athul K R <athulkr34@gmail.com>
2025-08-26 21:43:53 +0530

    Add files via upload

---
+++ areaofcircle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radius, area;
+    
+
+    cout << "Enter radius of the circle: ";
+    cin >> radius;
+
+    area = 22/7 * radius * radius;
+
+    cout << "Area of the circle = " << area << endl;
+
+    return 0;
+}
